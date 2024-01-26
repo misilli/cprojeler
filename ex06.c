e807c3830482474176fe76a0cd7ea59972458808
misilli <77497957+misilli@users.noreply.github.com>
2024-01-27 02:36:13 +0300

    asd

---
+++ ex06.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void isim()
+{
+	write(1,"deneme",1);
+	int a='0';
+	int b='0';
+	int c='0';
+	int d='0';
+
+	while (a<='9')
+	{
+		write(1.&a.1);
+		while (a<='9')
+		{
+			/* code */
+		}
+		
+		a++;
+
+	}
+	
+
+}
+int main()
+{
+	isim();
+}
