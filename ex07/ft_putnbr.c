a6e5fc8148474dfbdd80a5e281ec05c83cb9180c
misilli <77497957+misilli@users.noreply.github.com>
2024-01-29 02:29:18 +0300

    asd

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -1,59 +1,54 @@
 #include <unistd.h>
 
-void    ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void ft_putnbar(int nb)
+void	ft_putnbar(int nb)
 {
-    int dec;
-    
-    dec = nb ; 
-    int basamak_sayisi = 1;
-
-    if(nb<0)
-        dec=dec*-1;
-    
-    while (dec > 9) {
-        dec=dec/ 10; 
-        basamak_sayisi++;
-        
-    }
-
-    power(nb,basamak_sayisi);
+	int	dec;
+	int	basamak_sayisi;
+	dec = nb;
+	basamak_sayisi = 1;
+	if (nb < 0)
+		dec = dec * -1;
+	while (dec > 9)
+	{
+		dec = dec / 10;
+		basamak_sayisi++;
+	}
+	power(nb, basamak_sayisi);
 }
-void power(int nb,int basamak_sayisi)
+void	power(int nb, int basamak_sayisi)
 {
-    int j;
-    int power;
+	int	j;
+	int	power;
 
-    j=2;
-    power =1;
-    while ( basamak_sayisi >=j)
-    {
-        power = power *10;
-        j++;
-    }
-    giti(power, nb);
+	j = 2;
+	power = 1;
+	while (basamak_sayisi >= j)
+	{
+		power = power * 10;
+		j++;
+	}
+	giti(power, nb);
 }
-void giti (int b,int nb)
+void	giti(int b, int nb)
 {
-    if(nb<0)
-        ft_putchar('-');
-        nb=nb*-1;
-        
-    while (b>1)
-    {
-        ft_putchar(nb / b + 48);
-        nb = nb -  (nb / b)*b;
-        b=b/10;
-    }
-    ft_putchar(nb % 10 + 48);
+	if (nb < 0)
+		ft_putchar('-');
+	nb = nb * -1;
+	while (b > 1)
+	{
+		ft_putchar(nb / b + 48);
+		nb = nb - (nb / b) * b;
+		b = b / 10;
+	}
+	ft_putchar(nb % 10 + 48);
 }
 
-
-int main()
+int	main(void)
 {
-    ft_putnbar(0);
+	ft_putnbar(0);
 }
